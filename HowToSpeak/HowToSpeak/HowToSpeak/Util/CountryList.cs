f31bd0bb9bfca4f15b2931d21e84514ea70414fe
fumiya.kume <fumiya.kume@hotmail.com>
2016-10-02 05:19:19 +0900

    国コード関係のクラスを追加

---
+++ HowToSpeak/HowToSpeak/HowToSpeak/Util/CountryList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HowToSpeak.Util
+{
+    public static class CountryUtil
+    {
+        public static string GetCountryCode(Country country)
+        {
+            switch (country)
+            {
+                case Country.Japan:
+                    return "ja-JP";
+                case Country.US:
+                    return "us-US";
+                case Country.Chin:
+                    return "cn-ZN";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(country), country, null);
+            }
+        }
+    }
+
+    public enum Country
+    {
+        Japan,
+        US,
+        Chin
+    }
+}
