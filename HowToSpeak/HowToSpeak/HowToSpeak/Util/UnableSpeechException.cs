499d881afca88e0cfb894f9b8f8ce65ffd4312d2
fumiya.kume <fumiya.kume@hotmail.com>
2016-10-02 05:19:37 +0900

    音声合成できない場合の例外クラスを追加

---
+++ HowToSpeak/HowToSpeak/HowToSpeak/Util/UnableSpeechException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowToSpeak.Util
+{
+    public class UnableSpeechException : Exception
+    {
+        public UnableSpeechException()
+        {
+        }
+
+        public UnableSpeechException(string message) : base(message)
+        {
+        }
+
+        public UnableSpeechException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
